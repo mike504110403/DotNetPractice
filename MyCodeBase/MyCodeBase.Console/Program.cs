1ed32d67201d1cd2d1776c8b1e7dda55bb07162b
A-Ze <mike504110403@gmail.com>
2023-07-06 18:12:27 +0800

    [Library] 加入Extensions
    1. DateTimeExtension
    2. AsposeExtension

--- MyCodeBase/MyCodeBase.Console/Program.cs
+++ MyCodeBase/MyCodeBase.Console/Program.cs
@@ -7,48 +7,50 @@ using StackExchange.Redis;
 using System;
 using System.Collections.Specialized;
 using System.Configuration;
-using System.Reflection;
+using MyCodeBase.Library.Extensions;
+using MyCodeBase.Library.ViewModels.Test;
+using Aspose.Words;
 
 // 建立 reids 連線
-ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
-// 也可以一次連多個
-//ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("server1:6379,server2:6379");
-//string config = redis.Configuration;
+//ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
+//// 也可以一次連多個
+////ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("server1:6379,server2:6379");
+////string config = redis.Configuration;
 
-// 獲取 Redis 資料庫 
-var db = redis.GetDatabase();
+//// 獲取 Redis 資料庫 
+//var db = redis.GetDatabase();
 
-// 通過db使用Redis API （http://redis.io/commands）
-db.StringSet("mykey", "myvalue", new TimeSpan(0, 10, 0), When.Always, CommandFlags.None);
-// 創建連接到特定服務的 PUB/SUB 連接
-ISubscriber sub = redis.GetSubscriber();
-// 訂閱頻道，並處於監聽狀態，接受消息並處理
-var result = string.Empty;
-sub.Subscribe("messages", (channel, message) =>
-{
-    result = string.Format("Channel:{0} ; Message:{1} .", channel.ToString(), message);
-});
-// 在另一個進程或是機器上，發佈消息
-sub.Publish("messages", "hello");
+//// 通過db使用Redis API （http://redis.io/commands）
+//db.StringSet("mykey", "myvalue", new TimeSpan(0, 10, 0), When.Always, CommandFlags.None);
+//// 創建連接到特定服務的 PUB/SUB 連接
+//ISubscriber sub = redis.GetSubscriber();
+//// 訂閱頻道，並處於監聽狀態，接受消息並處理
+//var result = string.Empty;
+//sub.Subscribe("messages", (channel, message) =>
+//{
+//    result = string.Format("Channel:{0} ; Message:{1} .", channel.ToString(), message);
+//});
+//// 在另一個進程或是機器上，發佈消息
+//sub.Publish("messages", "hello");
 
-// 用 Key 找 Redis 內資料 
-var findKey = "mykey";
-var printKey = db.KeyExists(findKey)
-    ? db.KeyExists(findKey).ToString() + ": " + db.StringGet(findKey)
-    : findKey + " is not exist!";
-Console.WriteLine(printKey);
-// 寫入資料
-var Key = "myKey";
-var _lockKey = $"{"LockKey"}{Key}";
-if (!db.KeyExists(_lockKey))
-{
-    // 透過多寫一個key來表示在寫入中
-    db.StringSet(_lockKey, true.ToString());
-    db.StringSet(Key, "改寫後的");
-    // 移除標示用Key
-    db.KeyDelete(_lockKey);
-}
-Console.WriteLine(Key + ": " + db.StringGet(Key));
+//// 用 Key 找 Redis 內資料 
+//var findKey = "mykey";
+//var printKey = db.KeyExists(findKey)
+//    ? db.KeyExists(findKey).ToString() + ": " + db.StringGet(findKey)
+//    : findKey + " is not exist!";
+//Console.WriteLine(printKey);
+//// 寫入資料
+//var Key = "myKey";
+//var _lockKey = $"{"LockKey"}{Key}";
+//if (!db.KeyExists(_lockKey))
+//{
+//    // 透過多寫一個key來表示在寫入中
+//    db.StringSet(_lockKey, true.ToString());
+//    db.StringSet(Key, "改寫後的");
+//    // 移除標示用Key
+//    db.KeyDelete(_lockKey);
+//}
+//Console.WriteLine(Key + ": " + db.StringGet(Key));
 
 // 建container Builder
 //var builder = new ContainerBuilder();
@@ -72,3 +74,27 @@ Console.WriteLine(Key + ": " + db.StringGet(Key));
 //{
 //    var reader = container.Resolve<IConfigReader>();
 //}
+
+//var dateTime = DateTime.Now;
+//Console.WriteLine(dateTime.ToDateString());
+var t = 0;
+var testLists = new List<TestList>();
+while (t < 10)
+{
+    var testlist = new TestList()
+    {
+        Subject = "test",
+        Score = 100
+    };
+    testLists.Add(testlist);
+}
+var test = new Test()
+{
+    Name = "HHH",
+    Age = 26,
+    TestLists = testLists
+};
+// 開啟範例文檔
+var doc = new Document("D:\\MyPractice\\DotNetPractice\\MyCodeBase\\MyCodeBase.Console\\Temp\\test.docx");
+doc.BindData(test);
+doc.Save("D:\\MyPractice\\DotNetPractice\\MyCodeBase\\MyCodeBase.Console\\OutputFile\\bindedDoc.docx", SaveFormat.Docx);
--- MyCodeBase/MyCodeBase.Web/Controllers/HomeController.cs
+++ MyCodeBase/MyCodeBase.Web/Controllers/HomeController.cs
@@ -1,8 +1,14 @@
-﻿using System;
+﻿using MyCodeBase.Library.ViewModels.Test;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Xml.Linq;
+using Aspose.Words;
+using MyCodeBase.Library.Extensions;
+using Aspose.Words.Reporting;
 
 namespace MyCodeBase.Web.Controllers
 {
@@ -11,27 +17,43 @@ namespace MyCodeBase.Web.Controllers
         private NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
         public ActionResult Index()
         {
-            logger.Trace("**** Trace *** ");
-            logger.Debug("**** Debug ***");
-            logger.Info("**** Info ***");
-            logger.Warn("**** Warn ***");
-            logger.Error("**** Error ***");
-            logger.Fatal("**** Fatal ***");
+            //logger.Trace("**** Trace *** ");
+            //logger.Debug("**** Debug ***");
+            //logger.Info("**** Info ***");
+            //logger.Warn("**** Warn ***");
+            //logger.Error("**** Error ***");
+            //logger.Fatal("**** Fatal ***");
             return View();
         }
-
-        public ActionResult About()
+        /// <summary>
+        /// 取得doc檔匯出串流
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult ExportDocxFile()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            var t = 0;
+            var testLists = new List<TestList>();
+            while (t < 10)
+            {
+                var testlist = new TestList()
+                {
+                    Subject = "test",
+                    Score = 100
+                };
+                testLists.Add(testlist);
+                t += 1;
+            }
+            var data = new Test()
+            {
+                Name = "HHH",
+                Age = 26,
+                TestLists = testLists
+            };
+            // 開啟範例文檔
+            var doc = new Document("D:\\MyPractice\\DotNetPractice\\MyCodeBase\\MyCodeBase.Console\\Temp\\test.docx");
+            doc.BindData(data);
+            doc.Save("bindedDoc.docx", SaveFormat.Docx);
+            return File(doc.GetFileStream(SaveFormat.Docx), "application/docx");
         }
     }
 }
